1f8972781d3c0daa2e579f067db7c7a0c397e4d8
Vincent Cayadi <vincentcayadi45@gmail.com>
2021-06-30 22:00:12 +0800

    testing

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParsnioder.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.send("express is working")
+})
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000")
+})
