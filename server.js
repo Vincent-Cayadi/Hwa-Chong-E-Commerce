38b7f4ac093150c46b8d23d3cf8e604bc9edf4a9
Vincent Cayadi <vincentcayadi45@gmail.com>
2021-08-09 00:24:00 +0800

    Retriving Data From MongoDB works
    Have to change the DB thingy

--- server.js
+++ server.js
@@ -6,12 +6,26 @@ const ejs = require("ejs");
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb+srv://user:user@thicc-boy-production.kbt6v.mongodb.net/moviesDB?retryWrites=true&w=majority', {useNewUrlParser:true, useUnifiedTopology: true});
 
-app.get("/",function(req,res){
-    res.send("express is working")
+const moviesSchema = {
+    title : String,
+}
+
+const Movie = mongoose.model('Movie', moviesSchema);
+
+app.get('/',(req, res) => {
+    Movie.find({}, function(err, movies){
+        res.render('index',{
+            moviesList:movies
+        })
+    })
 })
 
-app.listen(3000, function(){
-    console.log("server is running on port 3000")
+app.listen(4000, function(){
+    console.log('server is running on port 4000, yes it works');
 })
+
+//https://stackoverflow.com/questions/50448272/avoid-current-url-string-parser-is-deprecated-warning-by-setting-usenewurlpars - Current URL string parser is deprecated
+//https://stackoverflow.com/questions/57895175/server-discovery-and-monitoring-engine-is-deprecated - Current Server Discover and Monitoring engine is deprecated
+// https://www.youtube.com/watch?v=yH593K9fYvE&t=777s
